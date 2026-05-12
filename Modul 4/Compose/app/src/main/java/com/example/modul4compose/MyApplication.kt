4bfd8e5eda97508ac863f82576176fbf496598e7
yuuukosoo <muhammadnaufal.nf59@gmail.com>
2026-05-12 23:05:00 +0800

    add

---
+++ Modul 4/Compose/app/src/main/java/com/example/modul4compose/MyApplication.kt
@@ -0,0 +1,13 @@
+package com.example.modul4compose
+
+import android.app.Application
+import timber.log.Timber
+
+class MyApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+
+        Timber.plant(Timber.DebugTree())
+    }
+}
